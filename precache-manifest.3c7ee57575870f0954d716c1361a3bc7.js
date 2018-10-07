5bfc726c627725b96f492ec116c57741485d763e
Stefano Masini <stefano@stefanomasini.com>
2018-10-07 22:47:09 +0200

    Updates

---
+++ precache-manifest.3c7ee57575870f0954d716c1361a3bc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7228ec382efd2f6c5d47",
+    "url": "/jigsaw-puzzle/static/js/runtime~main.7228ec38.js"
+  },
+  {
+    "revision": "5b35a5462bebb2c2db56",
+    "url": "/jigsaw-puzzle/static/js/main.5b35a546.chunk.js"
+  },
+  {
+    "revision": "d3b4abc441f99d6f752e",
+    "url": "/jigsaw-puzzle/static/js/1.d3b4abc4.chunk.js"
+  },
+  {
+    "revision": "5b35a5462bebb2c2db56",
+    "url": "/jigsaw-puzzle/static/css/main.3a4a2569.chunk.css"
+  },
+  {
+    "revision": "d3b4abc441f99d6f752e",
+    "url": "/jigsaw-puzzle/static/css/1.ffe41652.chunk.css"
+  },
+  {
+    "revision": "8e1ab542689c74ff79029ca792718ff0",
+    "url": "/jigsaw-puzzle/index.html"
+  }
+];
