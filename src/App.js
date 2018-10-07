6cb10245f5595ddacee487cda575fad0b2507716
Stefano Masini <stefano@stefanomasini.com>
2018-10-07 22:41:20 +0200

    cleaned up some unused react stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import seedrandom from 'seedrandom';
 
